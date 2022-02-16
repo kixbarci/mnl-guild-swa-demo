148e439513651abcc4cb8c6c6263dedf5baec3c3
Francis Barcebal <francis.barcebal@amadeus.com>
2022-02-16 13:41:05 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
-
+import {HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `<div>Hello {{value}}</div>`,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   value = 'World';
+
+  constructor(protected http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get('/api/helloworld')
+      .subscribe((data: any) => {
+        this.value = data?.message;
+      });
+  }
 }
